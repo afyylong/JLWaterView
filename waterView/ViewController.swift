5bddf03468382e483ee444b8f7371c8b7a8d9aa3
afyylong <afyylong@afyylongdeMacBook-Pro.local>
2017-04-02 15:04:21 +0800

    Initial Commit

---
+++ waterView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  waterView
+//
+//  Created by afyylong on 2017/4/2.
+//  Copyright © 2017年 afyylong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
